8ecabbe31524ae8a29a5eecab4a13a5d43213500
Aname Gloria <102767502+Aname326@users.noreply.github.com>
2024-03-28 10:15:46 +1300

    imported logo + change navbar color

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,9 +1,11 @@
 import '../styles.css'
+import Logo from '../assets/Hearts-Alight-Logo.png'
 
 export default function Navbar() {
     return (
         <div className='Navbar'>
-            navbar
+            <img src={Logo} />
+            
         </div>
     )
 }
