c48a8a99491021448131a4e2b8e0978b176d522c
Aname Gloria <102767502+Aname326@users.noreply.github.com>
2024-04-04 09:50:50 +1300

    putting in div so easy to manage

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -4,7 +4,9 @@ import Logo from '../assets/Hearts-Alight-Logo.png'
 export default function Navbar() {
     return (
         <div className='Navbar'>
-            <img src={Logo} /> 
+            <div className='Logo'>
+                <img src={Logo} /> 
+            </div>
             <div className='rightSide'></div>
         </div>
         
