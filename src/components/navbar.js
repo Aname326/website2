85cca6f4dd9787a90e9199805615b1daffd1329b
Aname Gloria <102767502+Aname326@users.noreply.github.com>
2024-04-12 09:55:14 +1200

    language for button changes

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,17 +1,52 @@
 import '../styles.css';
 import Logo from '../assets/Hearts-Alight-Logo.png';
 import {Link} from 'react-router-dom';
-
-let lang = 0;
-
-function Langchange() {
-    if (lang = 0) {
-        
-    }
-}
+import { useState } from 'react';
 
 export default function Navbar() {
-    let lang = 0;
+    const [lang, setLang] = useState({
+        About: "About",
+        Events: "Events",
+        Contacts: "Contacts",
+        Donations: "Donations"
+    }); 
+
+    const clickEng = () => {
+        setLang(previousState => {
+            return {
+                ...previousState,
+                About: "About",
+                Events: "Events",
+                Contacts: "Contacts",
+                Donations: "Donations"
+            }
+        })
+    }
+
+    const clickTC = () => {
+        setLang(previousState => {
+            return {
+                ...previousState,
+                About: "關於",
+                Events: "活動",
+                Contacts: "聯繫",
+                Donations: "捐獻"
+            }
+        })
+    }
+
+    const clickSC = () => {
+        setLang(previousState => {
+            return {
+                ...previousState,
+                About: "关于",
+                Events: "活动",
+                Contacts: "联系",
+                Donations: "捐献"
+            }
+        })
+    }
+
     return (
         <div className='Navbar'>
             <div className='Logo'>
@@ -24,18 +59,17 @@ export default function Navbar() {
             </div>
             <div className='rightSide'>
                  
-                <Link to={"/about"}> <div className='about'> About </div> </Link>
-                <Link to={"/events"}> <div className='events'> Events </div> </Link>
-                <Link to={"contacts"}> <div className='contacts'> Contacts </div> </Link>
-                <Link to={"/donations"}> <div className='donations'> Donations </div> </Link>
+                <Link to={"/about"}> <div className='about'> {lang.About} </div> </Link>
+                <Link to={"/events"}> <div className='events'> {lang.Events} </div> </Link>
+                <Link to={"contacts"}> <div className='contacts'> {lang.Contacts} </div> </Link>
+                <Link to={"/donations"}> <div className='donations'> {lang.Donations} </div> </Link>
             </div>
             <div className='language'>
-                <button className='eng'> English </button> <br />
-                <button className='TC'> 繁體中文 </button> <br />
-                <button className='SC'> 简体中文 </button>
+                <button className='eng' onClick={clickEng}> English </button> <br />
+                <button className='TC' onClick={clickTC}> 繁體中文 </button> <br />
+                <button className='SC' onClick={clickSC}> 简体中文 </button>
             </div>
         </div>
-        
     )
 }
 
