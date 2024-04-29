a265f04b9d1c7005c1067dcc64499a83b9d3bb5d
Aname Gloria <102767502+Aname326@users.noreply.github.com>
2024-04-29 12:44:09 +1200

    buttons for event posters

--- src/pages/events.js
+++ src/pages/events.js
@@ -3,7 +3,19 @@ import '../styles.css';
 export default function Events() {
     return (
         <div className='EventsPg'>
-            2024
+            2024 <br />
+            <button> April </button> <br />
+            <button> March </button> <br />
+            <button> February </button> <br />
+            <button> January </button> <br />
+            2023 <br />
+            <button> December </button> <br />
+            <button> November </button> <br />
+            <button> October </button> <br />
+            <button> September </button> <br />
+            <button> August </button> <br />
+            <button> July </button> <br />
+            <button> June </button> <br />
         </div>
     )
 }
