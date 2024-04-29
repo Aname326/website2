32cc232580c7f3b247947cf8e176a692f3e996cb
Aname Gloria <102767502+Aname326@users.noreply.github.com>
2024-04-29 13:02:06 +1200

    add another line break

--- src/pages/events.js
+++ src/pages/events.js
@@ -3,19 +3,20 @@ import '../styles.css';
 export default function Events() {
     return (
         <div className='EventsPg'>
-            2024 <br />
-            <button> April </button> <br />
-            <button> March </button> <br />
-            <button> February </button> <br />
-            <button> January </button> <br />
-            2023 <br />
-            <button> December </button> <br />
-            <button> November </button> <br />
-            <button> October </button> <br />
-            <button> September </button> <br />
-            <button> August </button> <br />
-            <button> July </button> <br />
-            <button> June </button> <br />
+            <h1> Events Posters </h1> <br />
+            2024 <br /> <br />
+            <button> April </button> <br /> <br />
+            <button> March </button> <br /> <br />
+            <button> February </button> <br /> <br />
+            <button> January </button> <br /> <br />
+            2023 <br /> <br />
+            <button> December </button> <br /> <br />
+            <button> November </button> <br /> <br />
+            <button> October </button> <br /> <br />
+            <button> September </button> <br /> <br />
+            <button> August </button> <br /> <br />
+            <button> July </button> <br /> <br />
+            <button> June </button> <br /> <br />
         </div>
     )
 }
