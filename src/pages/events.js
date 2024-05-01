69b03ac0f1505983dfd7041d07c61d31805c4da4
Aname Gloria <102767502+Aname326@users.noreply.github.com>
2024-05-02 09:30:15 +1200

    june posters

--- src/pages/events.js
+++ src/pages/events.js
@@ -2,8 +2,10 @@ import '../styles.css';
 import { useState } from 'react';
 import Navbar from '../components/navbar';
 
-import JulyEng from '../assets/JulyEnglish.png';
-import JulyChin from '../assets/JulyChinese.png';
+import JulyEng2023 from '../assets/JulyEnglish.png';
+import JulyChin2023 from '../assets/JulyChinese.png';
+import JuneEng2023 from '../assets/JuneEnglish.png';
+import JuneChin2023 from '../assets/JuneChinese.png';
 
 export default function Events() {
 
@@ -14,6 +16,16 @@ export default function Events() {
             setShowJuly(false)
         } else {
             setShowJuly(true)
+        }
+    }
+
+    const [showJune, setShowJune] = useState(false)
+
+    const ShowPosterJune = () => {
+        if (showJune == true) {
+            setShowJune(false)
+        } else {
+            setShowJune(true)
         }
     }
 
@@ -32,11 +44,13 @@ export default function Events() {
             <button> October </button> <br /> <br />
             <button> September </button> <br /> <br />
             <button> August </button> <br /> <br />
-            <button className='JulyBtn' onClick={ShowPosterJuly}> July </button> <br /> <br />
-            <p> {showJuly && <img src={JulyEng} />} </p>
-            <p> {showJuly && <img src={JulyChin} />} </p>
+            <button onClick={ShowPosterJuly}> July </button> <br /> <br />
+            <p> {showJuly && <img src={JulyEng2023} />} </p>
+            <p> {showJuly && <img src={JulyChin2023} />} </p>
         
-            <button> June </button> <br /> <br />
+            <button onClick={ShowPosterJune}> June </button> <br /> <br />
+            <p> {showJune && <img src={JuneEng2023} />} </p>
+            <p> {showJune && <img src={JuneChin2023} />} </p>
         </div>
     )
 }
