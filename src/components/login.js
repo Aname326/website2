934488ced44eb12d5a008579b867e2ac08efac65
Aname Gloria <102767502+Aname326@users.noreply.github.com>
2024-05-08 09:10:48 +1200

    title&2 inputs& login button

--- src/components/login.js
+++ src/components/login.js
@@ -6,7 +6,14 @@ export default function Login() {
         <div className='Login'>
             <img src={photo} />
             <div className='Block'>
-
+                <h1>
+                    Admin Login
+                </h1>
+                <p> ID: </p>
+                <input></input> <br />
+                <p> Password: </p>
+                <input></input> <br />
+                <button className='LoginBtn'> Login </button>
             </div>
         </div>
     )
