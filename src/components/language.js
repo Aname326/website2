6578212d5daad7326bd323dcec46f1b8f45c7f2d
Aname Gloria <102767502+Aname326@users.noreply.github.com>
2024-05-14 14:29:30 +1200

    translate in login component

--- src/components/language.js
+++ src/components/language.js
@@ -5,6 +5,7 @@ const StateContext = createContext();
 export const StateProvider = ({children}) => {
     const [lang, setLang] = useState({
       About: "About",
+      AdminLogin: "Admin Login",
       April: "April",
       August: "August",
       Contacts: "Contacts",
@@ -15,18 +16,21 @@ export const StateProvider = ({children}) => {
       EventsReg: "Events Registration",
       February: "February",
       HA: "Hearts Alight",
+      Login: "Login",
       March: "March",
       January: "January",
       July: "July",
       June: "June",
       November: "November",
       October: "October",
+      Password: "Password",
       September: "September",
     });
   
     const clickEng = () => {
       setLang({
         About: "About",
+        AdminLogin: "Admin Login",
         April: "April",
         August: "August",
         Contacts: "Contacts",
@@ -37,12 +41,14 @@ export const StateProvider = ({children}) => {
         EventsReg: "Events Registration",
         February: "February",
         HA: "Hearts Alight",
+        Login: "Login",
         March: "March",
         January: "January",
         July: "July",
         June: "June",
         November: "November",
         October: "October",
+        Password: "Password",
         September: "September",
       });
     };
@@ -50,6 +56,7 @@ export const StateProvider = ({children}) => {
     const clickTC = () => {
       setLang({
         About: "關於",
+        AdminLogin: "登入管理",
         April: "四月",
         August: "八月",
         Contacts: "聯繫",
@@ -60,12 +67,14 @@ export const StateProvider = ({children}) => {
         EventsReg: "活動報名",
         February: "二月",
         HA: "心燃青年團",
+        Login: "登入",
         March: "三月",
         January: "一月",
         July: "七月",
         June: "六月",
         November: "十一月",
         October: "十月",
+        Password: "密碼",
         September: "九月"
       });
     };
@@ -73,6 +82,7 @@ export const StateProvider = ({children}) => {
     const clickSC = () => {
       setLang({
         About: "关于",
+        AdminLogin: "登入管理",
         April: "四月",
         August: "八月",
         Contacts: "联系",
@@ -83,12 +93,14 @@ export const StateProvider = ({children}) => {
         EventsReg: "活动报名",
         February: "二月",
         HA: "心燃青年团",
+        Login: "登入",
         March: "三月",
         January: "一月",
         July: "七月",
         June: "六月",
         November: "十一月",
         October: "十月",
+        Password: "密码",
         September: "九月"
       });
     };
--- src/components/login.js
+++ src/components/login.js
@@ -1,19 +1,23 @@
 import '../styles.css';
 import photo from '../assets/2023CampPhoto.jpg';
+import { useStateContext } from './language';
 
 export default function Login() {
+
+    const { lang } = useStateContext()
+
     return (
         <div className='Login'>
             <img src={photo} />
             <div className='Block'>
                 <h1>
-                    Admin Login
+                    {lang.AdminLogin}
                 </h1>
                 <p> ID: </p>
                 <input></input> <br />
-                <p> Password: </p>
+                <p> {lang.Password} {':'} </p>
                 <input></input> <br />
-                <button> Login </button>
+                <button> {lang.Login} </button>
             </div>
         </div>
     )
