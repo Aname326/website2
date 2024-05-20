9e3f6d0bca8db667e88cb1021b4d53b9dd9f7d82
Aname Gloria <102767502+Aname326@users.noreply.github.com>
2024-05-20 14:13:17 +1200

    mission core statement and vision

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,7 +3,26 @@ import '../styles.css';
 export default function About() {
     return (
         <div className='AboutPg'>
-            about page
+            <h2>
+                Mission 
+            </h2>
+            <i>
+             {"Matthew 28:19 - 'Go therefore and make disciples of all nations, baptising them in the name of the Father and of the Son and of the Holy Spirit.'"}
+            </i>
+
+            <h2>
+                Vision 
+            </h2>
+            <i>
+             {"Matthew 5:13-14 - 'To be the Salt and Light of the world.'"}
+            </i>
+
+            <h2>
+                Core Statement
+            </h2>
+            <i>
+             {"John 13:34-35 - 'I give you a new commandment, that you love one another. Just as I have loved you, you also should love one another. By this everyone will know that you are my disciples, if you have love for one another.'"}
+            </i>
         </div>
     )
 }
