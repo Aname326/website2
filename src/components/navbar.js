ac3405aea6dfead34ce23b0180ce43f79feaab5e
Aname Gloria <102767502+Aname326@users.noreply.github.com>
2024-05-27 12:48:57 +1200

    remove comments uneccessary

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -7,57 +7,7 @@ export default function Navbar() {
 
     const { lang, clickEng, clickTC, clickSC } = useStateContext()
 
-    //const [lang, setLang] = useState({
-    //    HA: "Hearts Alight",
-    //    About: "About",
-    //    Events: "Events",
-    //    Contacts: "Contacts",
-    //    Donations: "Donations",
-    //    April: "April"
-    //}); 
-
-    //const clickEng = () => {
-    //    setLang(previousState => {
-    //        return {
-    //            ...previousState,
-    //            HA: "Hearts Alight",
-    //            About: "About",
-    //            Events: "Events",
-    //            Contacts: "Contacts",
-    //            Donations: "Donations",
-    //            April: "April"
-    //        }
-    //    })
-    //}
-
-    //const clickTC = () => {
-    //    setLang(previousState => {
-    //        return {
-    //            ...previousState,
-    //            HA: "心燃青年團",
-    //            About: "關於",
-    //            Events: "活動",
-    //            Contacts: "聯繫",
-    //            Donations: "捐獻",
-    //            April: "四月"
-    //        }
-    //    })
-    //}
-
-    //const clickSC = () => {
-    //    setLang(previousState => {
-    //        return {
-    //            ...previousState,
-    //            HA: "心燃青年团",
-    //            About: "关于",
-    //            Events: "活动",
-    //            Contacts: "联系",
-    //            Donations: "捐献",
-    //            April: "四月"
-    //        }
-    //    })
-    //}
-
+    
     return (
         <div className='Navbar'>
             <div className='Logo'>
