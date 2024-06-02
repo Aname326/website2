377e9f577bc30d9365b8c10dae30b10ec0a412a1
Aname Gloria <102767502+Aname326@users.noreply.github.com>
2024-06-03 11:32:05 +1200

    import salt & light into home pg

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,17 +1,14 @@
 import '../styles.css';
 
+import SaltLight from '../assets/salt&light.png';
+
 import MayEng2024 from '../assets/MayEnglish.png';
 import MayChin2024 from '../assets/MayChinese.png';
 
 export default function Home() {
     return (
         <div className='HomePg'>
-            <h1>
-                " Salt of the Earth 
-            </h1>
-            <h1>
-                Light of the World"
-            </h1>
+            <img className='SaltLight' src={SaltLight} />
 
             <br />
 
