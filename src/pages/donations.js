b6beb0489d4faf245926175045a23841691a7c6c
Aname Gloria <102767502+Aname326@users.noreply.github.com>
2024-06-05 09:10:30 +1200

    donation pg texts

--- src/pages/donations.js
+++ src/pages/donations.js
@@ -3,7 +3,19 @@ import '../styles.css';
 export default function Donations() {
     return (
         <div className='DonationsPg'>
-            donations page
+            <h1>
+                Thank You for your Kind Donations
+            </h1>
+
+            <img />
+
+            <h3>
+                Donating Informations
+            </h3>
+
+            <p>
+                
+            </p>
         </div>
     )
 }
