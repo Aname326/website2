0e1c0eed4a65581ba1f4bafb856cd0b37b85156e
Aname Gloria <102767502+Aname326@users.noreply.github.com>
2024-06-17 14:19:15 +1200

    firebase import authentication settings

--- src/components/firebase.js
+++ src/components/firebase.js
@@ -5,6 +5,7 @@ import { getAnalytics } from "firebase/analytics";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 import { getFirestore } from "@firebase/firestore"; 
+import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -22,3 +23,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 export const firestore = getFirestore(app);
+export const auth = getAuth(app);
