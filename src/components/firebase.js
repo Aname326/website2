0473885328d5634d044409319b653b107a24f345
Aname Gloria <102767502+Aname326@users.noreply.github.com>
2024-08-05 12:50:42 +1200

    import google auth provider

--- src/components/firebase.js
+++ src/components/firebase.js
@@ -5,7 +5,7 @@ import { getAnalytics } from "firebase/analytics";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 import { getFirestore } from "@firebase/firestore"; 
-import { getAuth } from "firebase/auth";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -24,3 +24,4 @@ const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 export const firestore = getFirestore(app);
 export const auth = getAuth(app);
+export const googleProvider = new GoogleAuthProvider();
