f0ed8636ffafe7aeaa5537cbebfcb4f1f6078c5a
Aname Gloria <102767502+Aname326@users.noreply.github.com>
2024-08-26 14:58:48 +1200

    testing for admin account

--- src/components/footnote.js
+++ src/components/footnote.js
@@ -1,5 +1,6 @@
 import '../styles.css';
 import { useStateContext } from './language';
+import { useState } from 'react';
 
 export default function Footnote() {
 
@@ -7,6 +8,20 @@ export default function Footnote() {
 
     const adminID = 'hayg@accc.org.nz'
     const adminPW = 'hayg1225'
+
+    const [adminIdInput, setAdminIdInput] = useState('')
+    const [adminPwInput, setAdminPwInput] = useState('')
+
+    const adminCheck = (e) => {
+        e.preventDefault();
+
+        if (adminIdInput == adminID && adminPwInput == adminPW) {
+            console.log("welcome admin :)")
+        } else {
+            console.log("you're not admin :(")
+        }
+        
+    }
 
     return (
         <div className='Footnote'>
@@ -20,12 +35,15 @@ export default function Footnote() {
                 </a>
             </div>
             <div className='Login'>
-                <h1> {lang.AdminLogin} </h1>
-                <p> ID: </p>
-                <input></input> <br />
-                <p> {lang.Password}: </p>
-                <input></input> <br />
-                <button> {lang.Login} </button>
+                <form onSubmit={adminCheck}>
+                    <h1> {lang.AdminLogin} </h1>
+                    <p> ID: </p>
+                    <input value={adminIdInput} onChange={(e) => setAdminIdInput(e.target.value)} ></input> <br />
+                    <p> {lang.Password}: </p>
+                    <input value={adminPwInput} onChange={(e) => setAdminPwInput(e.target.value)} type="password" ></input> <br />
+                    <button> {lang.Login} </button>
+                </form>
+                
             </div>
         </div>
     )
