aef49600a452045fe7980529fbad77564ab92e74
Aname Gloria <102767502+Aname326@users.noreply.github.com>
2024-08-26 14:01:23 +1200

    headings

--- src/pages/contacts.js
+++ src/pages/contacts.js
@@ -2,6 +2,9 @@ import '../styles.css';
 
 export default function Contacts() {
     return (
-        <div className='ContactsPg'></div>
+        <div className='ContactsPg'>
+            <h1>Youth Ministers</h1>
+            <h1>Youth Formators</h1>
+        </div>
     )
 }
