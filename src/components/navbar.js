d380d84936d1daafda81f1dde995d6a394da0882
Aname Gloria <102767502+Aname326@users.noreply.github.com>
2024-09-17 14:27:16 +1200

    move second row of navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -85,27 +85,37 @@ export default function Navbar() {
     }
     
     return (
-        <div className='Navbar'>
-            <div className='Logo'>
-                <Link to={"/"} onClick={clickHome}>
-                    <img src={Logo} /> 
-                </Link>
-            </div>
-            <div className='HA'>
-                {lang.HA}
-            </div>
-            <div className='rightSide'>
-                 
-                {lang.About && <Link to={"/about"} onClick={clickAbout} style={{'background-color': currentPage.aboutBgColor, 'color': currentPage.aboutColor}}> <div className='about'> {lang.About} </div> </Link>}
-                {lang.Events && <Link to={"/events"} onClick={clickEvents} style={{'background-color': currentPage.eventsBgColor, 'color': currentPage.eventsColor}}> <div className='events'> {lang.Events} </div> </Link>}
-                {lang.Contacts && <Link to={"contacts"} onClick={clickContacts} style={{'background-color': currentPage.contactsBgColor, 'color': currentPage.contactsColor}}> <div className='contacts'> {lang.Contacts} </div> </Link>}
-                {lang.Donations && <Link to={"/donations"} onClick={clickDonations} style={{'background-color': currentPage.donationsBgColor, 'color': currentPage.donationsColor}}> <div className='donations'> {lang.Donations} </div> </Link>}
-            </div>
-            <div className='language'>
-                <button className='eng' onClick={clickEng}> English </button> 
-                <button className='TC' onClick={clickTC}> 繁體中文 </button> 
-                <button className='SC' onClick={clickSC}> 简体中文 </button>
-            </div>
+        <div>
+            <div className='Navbar'>
+                <div className='Logo'>
+                    <Link to={"/"} onClick={clickHome}>
+                        <img src={Logo} /> 
+                    </Link>
+                </div>
+                <div className='HA'>
+                    {lang.HA}
+                </div>
+                <div className='rightSide'>
+                    {lang.About && <Link to={"/about"} onClick={clickAbout} style={{'background-color': currentPage.aboutBgColor, 'color': currentPage.aboutColor}}> <div className='about'> {lang.About} </div> </Link>}
+                    {lang.Events && <Link to={"/events"} onClick={clickEvents} style={{'background-color': currentPage.eventsBgColor, 'color': currentPage.eventsColor}}> <div className='events'> {lang.Events} </div> </Link>}
+                    {lang.Contacts && <Link to={"contacts"} onClick={clickContacts} style={{'background-color': currentPage.contactsBgColor, 'color': currentPage.contactsColor}}> <div className='contacts'> {lang.Contacts} </div> </Link>}
+                    {lang.Donations && <Link to={"/donations"} onClick={clickDonations} style={{'background-color': currentPage.donationsBgColor, 'color': currentPage.donationsColor}}> <div className='donations'> {lang.Donations} </div> </Link>}
+                </div>
+                <div className='language'>
+                    <button className='eng' onClick={clickEng}> English </button> 
+                    <button className='TC' onClick={clickTC}> 繁 </button> 
+                    <button className='SC' onClick={clickSC}> 简 </button>
+                </div>
+
+             </div>
+             <div className='smallNavbar'>
+                <div className='rightSideDropDown'>
+                    {lang.About && <Link to={"/about"} onClick={clickAbout} style={{'background-color': currentPage.aboutBgColor, 'color': currentPage.aboutColor}}> <div className='about'> {lang.About} </div> </Link>}
+                    {lang.Events && <Link to={"/events"} onClick={clickEvents} style={{'background-color': currentPage.eventsBgColor, 'color': currentPage.eventsColor}}> <div className='events'> {lang.Events} </div> </Link>}
+                    {lang.Contacts && <Link to={"contacts"} onClick={clickContacts} style={{'background-color': currentPage.contactsBgColor, 'color': currentPage.contactsColor}}> <div className='contacts'> {lang.Contacts} </div> </Link>}
+                    {lang.Donations && <Link to={"/donations"} onClick={clickDonations} style={{'background-color': currentPage.donationsBgColor, 'color': currentPage.donationsColor}}> <div className='donations'> {lang.Donations} </div> </Link>}
+                </div>
+             </div>
         </div>
     )
 }
