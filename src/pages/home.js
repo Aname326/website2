8a325a0339929b0b9060759719dfbe228ec86d94
Aname Gloria <102767502+Aname326@users.noreply.github.com>
2024-10-08 22:13:20 +1300

    hide poster upload stuff

--- src/pages/home.js
+++ src/pages/home.js
@@ -36,16 +36,16 @@ export default function Home() {
 
             <div className='Posters'>
                 <img src={MayEng2024} /> 
-                
-                <input type="file" accept='image/jpg, image/png, image/jpeg' onChange={(e) => setPosterUpload(e.target.files[0])} />
-                <button onClick={uploadPoster} > Replace </button>
-                
+                <div className='Upload'>
+                    <input type="file" accept='image/jpg, image/png, image/jpeg' onChange={(e) => setPosterUpload(e.target.files[0])} />
+                    <button onClick={uploadPoster} > Replace </button>
+                </div>
                 <br />
                 <img src={MayChin2024} />
-                
-                <input type="file" accept='image/jpg, image/png, image/jpeg' onChange={(e) => setPosterUpload(e.target.files[0])} />
-                <button onClick={uploadPoster} > Replace </button>
-                
+                <div className='Upload'>
+                    <input type="file" accept='image/jpg, image/png, image/jpeg' onChange={(e) => setPosterUpload(e.target.files[0])} />
+                    <button onClick={uploadPoster} > Replace </button>
+                </div>
             </div>
         </div>
     )
